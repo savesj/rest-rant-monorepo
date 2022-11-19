b8fdac165bed25a7af7472ec8ba8e670df5a6d5f
savesj <jesuslares.20@hotmail.com>
2022-11-19 12:20:07 -0500

    Fixing CurrentUser

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -8,13 +8,13 @@ function CurrentUserProvider({ children }){
     const [currentUser, setCurrentUser] = useState(null)
     useEffect(() => {
         const getLoggedInUser = async () => {
-            let response = fetch('http://localhost:5000/authentication/profile')
+            let response = await fetch('http://localhost:5000/authentication/profile')
             let user = await response.json()
             setCurrentUser(user)
         }
         getLoggedInUser()
     }, [])
-    
+
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
             {children}
