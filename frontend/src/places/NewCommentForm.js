c015b58803c77523a34a5c5583180a0a5a6fab69
savesj <jesuslares.20@hotmail.com>
2022-11-19 20:03:36 -0500

    Hidding newcomentForm

--- frontend/src/places/NewCommentForm.js
+++ frontend/src/places/NewCommentForm.js
@@ -36,6 +36,12 @@ function NewCommentForm({ place, onSubmit }) {
             authorId: authors[0]?.userId
         })
     }
+    const { currentUser } = useContext(CurrentUser)
+
+    if(!currentUser){
+        return <p>You must be logged in to leave a rant or rave.</p>
+    }
+
 
     return (
         <form onSubmit={handleSubmit}>
